1c1b091257519c43912aa4dff1c721115a5c41e1
Justin Shin <shine777justin@gmail.com>
2024-09-12 11:39:11 +0000

    build with ccache

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-make -j$(nproc)
+# https://nickdesaulniers.github.io/blog/2018/06/02/speeding-up-linux-kernel-builds-with-ccache/
+make CC="ccache gcc" -j$(nproc)
 sudo make modules_install
 sudo make install
 sudo update-grub
